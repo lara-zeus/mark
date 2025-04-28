5f6bfbc2345632f7204f649e50d12fe1a5082f8b
Ash Monsh <wh7r.com@gmail.com>
2025-04-28 15:31:54 +0300

    Update mark.blade.php

--- resources/views/forms/components/mark.blade.php
+++ resources/views/forms/components/mark.blade.php
@@ -1,46 +1,55 @@
+@php
+    $statePath = $getStatePath();
+    $icons = $getIcons();
+    $selectedIcons = $getSelectedIcons();
+    $isSequential = $isSequential();
+    $iconsKeys = array_keys($icons);
+    // todo move to the component or on the facade
+    $showSelectedValues = function($key) use ($isSequential, $iconsKeys) {
+        if(!$isSequential){
+            return [(string)$key];
+        }
+        $keyIndex = array_search($key, $iconsKeys);
+        $iconsKeys =  array_filter($iconsKeys, fn($key) => $key >= $keyIndex, ARRAY_FILTER_USE_KEY);
+
+        return array_values(array_map(fn($value) => (string) $value, $iconsKeys));
+    };
+@endphp
 <x-dynamic-component
     :component="$getFieldWrapperView()"
     :field="$field"
 >
-    @php
-        $statePath = $getStatePath();
-        $icons = $getIcons();
-        $selectedIcons = $getSelectedIcons();
-        $isSequential = $isSequential();
-        $keys = array_keys($icons);
-        $showSelectedValues = function($key) use ($isSequential, $keys) {
-            if(!$isSequential){
-                return [(string)$key];
-            }
-            $keyIndex = array_search($key, $keys);
-            $keys =  array_filter($keys, fn($key) => $key >= $keyIndex, ARRAY_FILTER_USE_KEY);
-
-            return array_values(array_map(fn($value) => (string) $value, $keys));
-        };
-    @endphp
-    <div x-data="{
-        state: $wire.{{ $applyStateBindingModifiers("\$entangle('{$statePath}')") }}
-    }" class="flex flex-wrap gap-5">
+    <div
+        x-data="{
+            state: $wire.{{ $applyStateBindingModifiers("\$entangle('{$statePath}')") }}
+        }"
+        class="flex flex-wrap gap-5"
+    >
         @foreach($icons as $key => $icon)
             @php
                 $values = json_encode($showSelectedValues($key));
             @endphp
             <div>
-                <input type="{{ count($icons) > 1 ? 'radio' : 'checkbox' }}" name="{{ $statePath }}" x-model="state"
-                       :value="'{{ $key }}'" class="hidden">
+                <input
+                    name="{{ $statePath }}"
+                    type="{{ count($icons) > 1 ? 'radio' : 'checkbox' }}"
+                    x-model="state"
+                    value="{{ $key }}"
+                    class="hidden"
+                >
                 <x-filament::icon-button
+                    :icon="$selectedIcons[$key]"
+                    :class="__('filament-panels::layout.direction') === 'rtl' ? '-scale-x-100' : ''"
                     x-on:click="state = state === '{{ $key }}' ? null : '{{ $key }}'"
                     x-show="{{ $values }}.includes(state)"
-                    icon="{{ $selectedIcons[$key] }}"
                     size="xl"
-                    :class="__('filament-panels::layout.direction') === 'rtl' ? '-scale-x-100' : ''"
                 />
                 <x-filament::icon-button
+                    :class="__('filament-panels::layout.direction') === 'rtl' ? '-scale-x-100' : ''"
+                    :icon="$icon"
                     x-on:click="state = state === '{{ $key }}' ? null : '{{ $key }}'"
                     x-show="!{{ $values }}.includes(state)"
-                    icon="{{ $icon }}"
                     size="xl"
-                    :class="__('filament-panels::layout.direction') === 'rtl' ? '-scale-x-100' : ''"
                 />
             </div>
         @endforeach
