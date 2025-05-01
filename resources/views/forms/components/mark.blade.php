f221d869dd28ad2c42f43956b06e4d947a49fe1b
Ash Monsh <wh7r.com@gmail.com>
2025-05-01 21:25:48 +0300

    Update mark.blade.php

--- resources/views/forms/components/mark.blade.php
+++ resources/views/forms/components/mark.blade.php
@@ -1,30 +1,33 @@
-<x-dynamic-component
-    :component="$getFieldWrapperView()"
-    :field="$field"
->
-    @php
-        $statePath = $getStatePath();
-        $icons = $getIcons();
-        $selectedIcons = $getSelectedIcons();
-        $isSequential = $isSequential();
-        $iconsKeys = array_keys($icons);
-        // todo move to the component or on the facade
-        $showSelectedValues = function($key) use ($isSequential, $iconsKeys) {
-            if(!$isSequential){
-                return [(string)$key];
-            }
-            $keyIndex = array_search($key, $iconsKeys);
-            $iconsKeys =  array_filter($iconsKeys, fn($key) => $key >= $keyIndex, ARRAY_FILTER_USE_KEY);
+@php
+    $statePath = $getStatePath();
+    $icons = $getIcons();
+    $selectedIcons = $getSelectedIcons();
+    $isSequential = $isSequential();
+    $iconsKeys = array_keys($icons);
+    // todo move to the component or on the facade?
+    $showSelectedValues = function($key) use ($isSequential, $iconsKeys) {
+        if(!$isSequential){
+            return [(string)$key];
+        }
+        $keyIndex = array_search($key, $iconsKeys, true);
+        $iconsKeys =  array_filter($iconsKeys, fn($key) => $key >= $keyIndex, ARRAY_FILTER_USE_KEY);
 
-            return array_values(array_map(fn($value) => (string) $value, $iconsKeys));
-        };
-    @endphp
-    <div x-data="{
-        state: $wire.{{ $applyStateBindingModifiers("\$entangle('{$statePath}')") }}
-        }" class="flex flex-wrap gap-5">
+        return array_values(array_map(fn($value) => (string) $value, $iconsKeys));
+    };
+@endphp
+<x-dynamic-component
+        :component="$getFieldWrapperView()"
+        :field="$field"
+>
+    <div
+            x-data="{
+            state: $wire.{{ $applyStateBindingModifiers("\$entangle('{$statePath}')") }}
+        }"
+            class="flex flex-wrap gap-5"
+    >
         @foreach($icons as $key => $icon)
             @php
-                $values = json_encode($showSelectedValues($key));
+                $values = json_encode($showSelectedValues($key), JSON_THROW_ON_ERROR);
             @endphp
             <div>
                 <input
