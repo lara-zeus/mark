0cd351dd0d2aa92252811baea214e0e5d2e67021
Abdulmajeed-Jamaan <41128358+Abdulmajeed-Jamaan@users.noreply.github.com>
2025-05-10 14:40:18 +0300

    fix(rating): handle deprecation

--- src/Traits/Rating/HasRatings.php
+++ src/Traits/Rating/HasRatings.php
@@ -23,14 +23,6 @@ trait HasRatings
     /**
      * @throws Throwable
      */
-    public function hasRated(Model $markable): bool
-    {
-        return $this->hasMarked('ratings', $markable);
-    }
-
-    /**
-     * @throws Throwable
-     */
     public function unmarkRating(Model $markable)
     {
         return $this->unmark('ratings', $markable);
@@ -42,5 +34,22 @@ trait HasRatings
     public function rate(Model $markable, int $value, array | null | NotPassed $metadata = new NotPassed): Model
     {
         return $this->mark('ratings', $markable, $value, $metadata);
+    }
+
+    /**
+     * @deprecated will be replaced with local scopes
+     */
+    public function hasRated(Model $model): bool
+    {
+        return $this->ratings()->whereMorphedTo('markable', $model)->exists();
+    }
+
+    /**
+     * @throws Throwable
+     * @deprecated for better naming alternative, use rate() instead
+     */
+    public function markRating(Model $markable, int $value, array | null | NotPassed $metaData = new NotPassed): Model
+    {
+        return $this->mark('ratings', $markable, $value, $metaData);
     }
 }
--- src/Traits/Rating/Rateable.php
+++ src/Traits/Rating/Rateable.php
@@ -4,6 +4,7 @@ namespace LaraZeus\Mark\Traits\Rating;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 use LaraZeus\Mark\Facades\Mark;
 use LaraZeus\Mark\NotPassed;
 use LaraZeus\Mark\Traits\Markable;
@@ -34,12 +35,9 @@ trait Rateable
         return $this->morphMany(Mark::getRatingMorphPivotModel(), 'markable');
     }
 
-    /**
-     * @throws Throwable
-     */
-    public function hasRatedBy(Model $marker): bool
+    public function rating(): MorphOne
     {
-        return $this->hasMarkedBy('ratings', $marker);
+        return $this->morphOne(Mark::getRatingMorphPivotModel(), 'markable');
     }
 
     /**
@@ -56,5 +54,25 @@ trait Rateable
     public function rateBy(Model $marker, int $value, array | null | NotPassed $metadata = new NotPassed): Model
     {
         return $this->markBy('ratings', $marker, $value, $metadata);
+    }
+
+    /**
+     * @deprecated will be replaced with local scopes
+     */
+    public function isRatedBy(Model $marker): bool
+    {
+        return $this->ratings()
+            ->whereBelongsTo($marker, 'marker')
+            ->where('value', true)
+            ->exists();
+    }
+
+    /**
+     * @throws Throwable
+     * @deprecated for better naming alternative, use rateBy() instead.
+     */
+    protected function markRating(Model $marker, int $value, array | null | NotPassed $metaData = new NotPassed): Model
+    {
+        return $this->markBy('ratings', $marker, $value, $metaData);
     }
 }
