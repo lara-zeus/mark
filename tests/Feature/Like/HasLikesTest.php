d1ae35ee082c1bcbcd01fe8a2cfe525bb9d1e736
Abdulmajeed-Jamaan <41128358+Abdulmajeed-Jamaan@users.noreply.github.com>
2025-05-16 00:25:23 +0300

    test: add missing marker scopes tests

--- tests/Feature/Like/HasLikesTest.php
+++ tests/Feature/Like/HasLikesTest.php
@@ -65,4 +65,122 @@ describe('scope', function () {
                 ->not->toContainModel($this->marker1);
         });
     });
+
+    describe('whereLiked', function () {
+        beforeEach(function () {
+            $this->marker1 = Marker::factory()->create();
+            $this->markables1 = Markable::factory()
+                ->count(3)
+                ->create()
+                ->each(
+                    fn (Markable $markable) => $markable
+                        ->likedBy()
+                        ->attach($this->marker1, ['value' => true])
+                );
+
+            $this->marker2 = Marker::factory()->create();
+            $this->markables2 = Markable::factory()
+                ->count(3)
+                ->create()
+                ->each(
+                    fn (Markable $markable) => $markable
+                        ->likedBy()
+                        ->attach($this->marker2, ['value' => true])
+                );
+        });
+
+        test('has correct signature', function () {
+            $method = (new ReflectionClass(Marker::class))
+                ->getMethod('scopeWhereLiked');
+
+            expect($method->isPublic())->toBeTrue();
+
+            $parameters = $method->getParameters();
+            expect($parameters)->toHaveCount(2);
+
+            [$queryParam, $markableParam] = $parameters;
+
+            expect($queryParam->getName())->toBe('query')
+                ->and($queryParam->getType()->getName())->toBe(Builder::class)
+                ->and($queryParam->getType()->allowsNull())->toBeFalse();
+
+            expect($markableParam->getName())->toBe('markable')
+                ->and($markableParam->getType())->toBeInstanceOf(ReflectionUnionType::class)
+                ->and(collect($markableParam->getType()->getTypes())->map(fn ($t) => $t->getName())->all())
+                ->toMatchArray([Model::class, Collection::class]);
+        });
+
+        test('filter the relations currectly', function () {
+            expect(Marker::whereLiked($this->markables1)->get())
+                ->toHaveCount(1)
+                ->toContainModel($this->marker1);
+
+            expect(Marker::whereLiked($this->markables2)->get())
+                ->toHaveCount(1)
+                ->toContainModel($this->marker2);
+
+            expect(Marker::whereLiked($this->markables2)->get())
+                ->toHaveCount(1)
+                ->not->toContainModel($this->marker1);
+        });
+    });
+
+    describe('whereDisliked', function () {
+        beforeEach(function () {
+            $this->marker1 = Marker::factory()->create();
+            $this->markables1 = Markable::factory()
+                ->count(3)
+                ->create()
+                ->each(
+                    fn (Markable $markable) => $markable
+                        ->likedBy()
+                        ->attach($this->marker1, ['value' => false])
+                );
+
+            $this->marker2 = Marker::factory()->create();
+            $this->markables2 = Markable::factory()
+                ->count(3)
+                ->create()
+                ->each(
+                    fn (Markable $markable) => $markable
+                        ->likedBy()
+                        ->attach($this->marker2, ['value' => false])
+                );
+        });
+
+        test('has correct signature', function () {
+            $method = (new ReflectionClass(Marker::class))
+                ->getMethod('scopeWhereDisliked');
+
+            expect($method->isPublic())->toBeTrue();
+
+            $parameters = $method->getParameters();
+            expect($parameters)->toHaveCount(2);
+
+            [$queryParam, $markableParam] = $parameters;
+
+            expect($queryParam->getName())->toBe('query')
+                ->and($queryParam->getType()->getName())->toBe(Builder::class)
+                ->and($queryParam->getType()->allowsNull())->toBeFalse();
+
+            expect($markableParam->getName())->toBe('markable')
+                ->and($markableParam->getType())->toBeInstanceOf(ReflectionUnionType::class)
+                ->and(collect($markableParam->getType()->getTypes())->map(fn ($t) => $t->getName())->all())
+                ->toMatchArray([Model::class, Collection::class]);
+        });
+
+        test('filter the relations currectly', function () {
+            expect(Marker::whereDisliked($this->markables1)->get())
+                ->toHaveCount(1)
+                ->toContainModel($this->marker1);
+
+            expect(Marker::whereDisliked($this->markables2)->get())
+                ->toHaveCount(1)
+                ->toContainModel($this->marker2);
+
+            expect(Marker::whereDisliked($this->markables2)->get())
+                ->toHaveCount(1)
+                ->not->toContainModel($this->marker1);
+        });
+    });
 });
