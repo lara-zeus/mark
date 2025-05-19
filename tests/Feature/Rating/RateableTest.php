c817cbf261c70cf66c139ed6390c48dc6153eb21
Abdulmajeed-Jamaan <41128358+Abdulmajeed-Jamaan@users.noreply.github.com>
2025-05-19 23:30:50 +0300

    test(rating): add relation tests

--- tests/Feature/Rating/RateableTest.php
+++ tests/Feature/Rating/RateableTest.php
@@ -3,8 +3,92 @@
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use LaraZeus\Mark\Models\MarkRate;
 use LaraZeus\Mark\Tests\Models\Markable;
 use LaraZeus\Mark\Tests\Models\Marker;
+
+describe('relation', function () {
+    describe('ratedBy', function () {
+        test('it has correct signature', function () {
+            $marker = new Markable;
+            expect(method_exists($marker, 'ratedBy'))->toBeTrue();
+            expect($marker->ratedBy())->toBeInstanceOf(MorphToMany::class);
+        });
+
+        test('it can retrieve currect records via relation', function () {
+            $marker = Marker::factory()->create();
+            $markable = Markable::factory()->create();
+
+            $mark = MarkRate::create([
+                'value' => true,
+                'marker_id' => $marker->getKey(),
+                'markable_id' => $markable->getKey(),
+                'markable_type' => $markable->getMorphClass(),
+
+            ]);
+
+            expect($markable->ratedBy()->get())
+                ->toHaveCount(1)
+                ->toContainModel($marker);
+        })
+            ->depends('it has correct signature');
+    });
+
+    describe('ratings', function () {
+        test('it has correct signature', function () {
+            $marker = new Markable;
+            expect(method_exists($marker, 'ratings'))->toBeTrue();
+            expect($marker->ratings())->toBeInstanceOf(MorphMany::class);
+        });
+
+        test('it can retrieve currect records via relation', function () {
+            $marker = Marker::factory()->create();
+            $markable = Markable::factory()->create();
+
+            $mark = MarkRate::create([
+                'value' => true,
+                'marker_id' => $marker->getKey(),
+                'markable_id' => $markable->getKey(),
+                'markable_type' => $markable->getMorphClass(),
+
+            ]);
+
+            expect($markable->ratings()->get())
+                ->toHaveCount(1)
+                ->toContainModel($mark);
+        })
+            ->depends('it has correct signature');
+    });
+
+    describe('rating', function () {
+        test('it has correct signature', function () {
+            $marker = new Markable;
+            expect(method_exists($marker, 'rating'))->toBeTrue();
+            expect($marker->rating())->toBeInstanceOf(MorphOne::class);
+        });
+
+        test('it can retrieve currect records via relation', function () {
+            $marker = Marker::factory()->create();
+            $markable = Markable::factory()->create();
+
+            $mark = MarkRate::create([
+                'value' => true,
+                'marker_id' => $marker->getKey(),
+                'markable_id' => $markable->getKey(),
+                'markable_type' => $markable->getMorphClass(),
+
+            ]);
+
+            expect($markable->rating()->get())
+                ->toHaveCount(1)
+                ->toContainModel($mark);
+        })
+            ->depends('it has correct signature');
+    });
+});
 
 describe('scope', function () {
     beforeEach(function () {
