42bb1c5e7c0d111594eb9eb86426eea9830f259d
Abdulmajeed-Jamaan <41128358+Abdulmajeed-Jamaan@users.noreply.github.com>
2025-08-29 17:21:47 +0300

    Direct redirect to the component

--- demo/routes/web.php
+++ demo/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Filament\Resources\CommentResource\Pages\EditComment;
+use App\Models\Comment;
 use App\Models\User;
 use Filament\Facades\Filament;
 use Illuminate\Support\Facades\Route;
@@ -7,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     Filament::auth()->login(User::first());
 
-    return redirect()->to(Filament::getDefaultPanel()->getUrl());
+    return redirect()->to(EditComment::getUrl([Comment::first()]));
 })->name('home');
 
 Route::redirect('/admin/login', '/');
