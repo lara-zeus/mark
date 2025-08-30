2a966c0c1a25119652ae84f63ae677ad4f7cd98f
Abdulmajeed-Jamaan <41128358+Abdulmajeed-Jamaan@users.noreply.github.com>
2025-08-30 04:15:53 +0000

    Fix styling

--- demo/app/Models/Comment.php
+++ demo/app/Models/Comment.php
@@ -12,11 +12,11 @@ use LaraZeus\Mark\Traits\Rating\Rateable;
 class Comment extends Model
 {
     use Bookmarkable;
-
     use Bookmarkable;
 
     /** @use HasFactory<\Database\Factories\CommentFactory> */
     use HasFactory;
+
     use Likeable;
     use Rateable;
 
